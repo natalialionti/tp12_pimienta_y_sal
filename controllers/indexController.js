4a1829e46b31e3600f370165441bbd33af2ef594
“mi <nataliasaxo@gmail.com>
2022-08-05 08:38:01 -0300

    segundo commit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,11 +2,19 @@ const products = require("../data/productsData");
 
 module.exports = {
 
-  index: function(req, res) { 
-    
- res.render('index', { 
+  index: (req, res) => {
+    return res.render("index", {
+      title: "Home | Pimienta y sal",
+      products,
+    });
+  },
+};
+ 
+  
+ /* index: function(req, res) { 
+    res.render('index', { 
     title:'Home | Pimienta y Sal',
     products: products,
   })  
   
-}} 
+}} */
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,21 @@
 const products = require("../data/productsData");
 
 module.exports = {
-    detail: function(req, res) { 
+    detail: (req, res) => {
+      let product = products.find((product) => product.slug === req.params.slug);
+  
+      return res.render("detalleMenu", {
+        product
+      });
+    },
+  }; 
+    
+    /* detail : function(req, res) { 
        
-      let product = products.find(product => product.id === +req.params.id);
+      let product = products.find((product) => product.id === +req.params.id);
 
-      return  res.render('detalleMenu', { product })
-    }  
+      return  res.render('detalleMenu', {product})
+    } ,
 
   }   /* renderizo la vista detalle menu, osea, el controlador va a mostrar lo que haya en detalleMenu.js*/
       
